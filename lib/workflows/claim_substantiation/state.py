5ac40113b41af3b8587f4171f9da4328b00f13ea
OmidS <omidsani@gmail.com>
2025-09-11 14:27:08 -0700

    minor cleanup

--- lib/workflows/claim_substantiation/state.py
+++ lib/workflows/claim_substantiation/state.py
@@ -2,7 +2,8 @@ from typing import List, TypedDict
 
 from lib.agents.citation_detector import CitationResponse
 from lib.agents.claim_detector import ClaimResponse
-from lib.agents.reference_matcher import ReferenceMatch
+from lib.agents.reference_extractor import BibliographyItem
+from lib.agents.claim_substantiator import ClaimSubstantiationResult
 from lib.services.file import File
 
 
@@ -13,6 +14,5 @@ class ClaimSubstantiatorState(TypedDict, total=False):
     # Outputs
     claims_by_chunk: List[ClaimResponse]
     citations_by_chunk: List[CitationResponse]
-    references: List[str]
-    matches: List[ReferenceMatch]
-    claim_substantiations_by_chunk: List[List[ReferenceMatch]]
+    references: List[BibliographyItem]
+    claim_substantiations_by_chunk: List[List[ClaimSubstantiationResult]]
