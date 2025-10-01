79429dfab73c5b6d0577fc818f200663dfd3da23
OmidS <omidsani@gmail.com>
2025-10-01 03:08:43 -0700

    Add ClaimSubstantiationRequest model export to API models
    - Included `ClaimSubstantiationRequest` in the index of generated API models to enhance the claim substantiation workflow.
    - This addition supports the integration of new features related to claim substantiation requests in the frontend.

--- frontend/lib/generated-api/models/index.ts
+++ frontend/lib/generated-api/models/index.ts
@@ -11,6 +11,7 @@ export * from './CitationType';
 export * from './Claim';
 export * from './ClaimCommonKnowledgeResultWithClaimIndex';
 export * from './ClaimResponse';
+export * from './ClaimSubstantiationRequest';
 export * from './ClaimSubstantiationResultWithClaimIndex';
 export * from './ClaimSubstantiatorStateInput';
 export * from './ClaimSubstantiatorStateOutput';
